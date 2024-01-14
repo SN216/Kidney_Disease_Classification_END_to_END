16866a537e686df984ff9fdb1086337d2f33c4fd
SN <snbibhudutta216@gmail.com>
2024-01-14 20:15:52 +0530

    model evaluation added and mlflow added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,9 +1,10 @@
 from cnnClassifier.constants import *
 import os
-from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.utils.common import read_yaml, create_directories, save_json
 from cnnClassifierentity.config_entity import (DataIngestionConfig,
                                                PrepareBaseModelConfig,
-                                               TrainingConfig)
+                                               TrainingConfig,
+                                               EvaluationConfig)
 class ConfigurationManager:
     #DEFINING A SPECIAL FUNCTION FOR INITIALIZING OBECTS
     def __init__(
@@ -69,3 +70,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/SN216/Kidney_Disease_Classification_END_to_END.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
